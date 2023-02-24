f07882157a3b5c2f316c6ef92cb94105d8e33353
viethung204 <viethungnguyen01@gmail.com>
2023-02-24 07:55:37 +0700

    bug fixes

--- meteoroids!/Assets/scripts/inGame/PauseMenu/Pause.cs
+++ meteoroids!/Assets/scripts/inGame/PauseMenu/Pause.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Pause : MonoBehaviour
 {
@@ -9,6 +10,13 @@ public class Pause : MonoBehaviour
     public GameObject pauseMenuUI;
     public GameObject main;
     public GameObject options;
+
+    public GameObject resumeTri;
+    public GameObject optionsTri;
+    public GameObject Q2MTri;
+    public GameObject QuitTri;
+
+    public GameObject resumeButton;
 
     // Start is called before the first frame update
     void Start()
@@ -43,6 +51,13 @@ public class Pause : MonoBehaviour
     public void Pausing()
     {
         pauseMenuUI.SetActive(true);
+        main.SetActive(true);
+        options.SetActive(false);
+        resumeTri.SetActive(true);
+        optionsTri.SetActive(false);
+        Q2MTri.SetActive(false);
+        QuitTri.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(resumeButton);
         Time.timeScale = 0f;
         isPaused = true;
     }
